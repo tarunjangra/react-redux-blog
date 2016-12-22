5375fe6f59ba2c79f279fd156305e48008baa39b
Tarun Jangra <tarun@izap.in>
2016-12-22 17:16:25 +0530

    added reselect

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
+    console.log(window);
     return (
       <div>
         {this.props.children}
---
+++ src/selectors/selected_posts.js
@@ -0,0 +1,7 @@
+import { createSelector } from 'reselect';
+
+
+//create select function to take care of pieces of state we care about.
+// for this calculation
+
+
