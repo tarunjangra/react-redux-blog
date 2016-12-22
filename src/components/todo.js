f7c39cb6944f366f6eaeb2106936e1ebc2389ffd
Tarun Jangra <tarun@izap.in>
2016-12-23 01:08:46 +0530

    Animation of list element with react

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,48 +1,43 @@
 import _ from 'lodash';
 import React,{Component} from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router';
+import Faker from 'faker';
+import ReactCSSTrasitionGroup from 'react-addons-css-transition-group';
 
 export default class ToDo extends Component {
     
     constructor(props) {
         super(props)
-        this.state = {items: []};
-        this.renderList = this.renderList.bind(this);
+        this.state = {quotes: []};
   }
 
     onAddList() {
-        this.setState({
-            items: this.state.items.concat([_.now()])
-        });
+        const quote = Faker.lorem.sentence();
+        this.setState({quotes: [...this.state.quotes, quote]});
     }
 
     renderList(){
-        return this.state.items.map((value) => {
-            return <li className="list-group-item" key={value}>
-                {value}<button className="btn pull-right btn-danger no-padding" onClick={this.removeList.bind(this)}>Remove</button>
+        return this.state.quotes.map((item, index) => {
+            return <li className="list-group-item" key={index}>
+                {item}<button className="btn pull-right btn-danger no-padding" onClick={this.removeList.bind(this,item)}>Remove</button>
             </li>
          });
     }
-    removeList(){
-        var item = this.state.items;
-        item.splice(-1);
-        this.setState({
-            items: item
-        });
+    removeList(quote){
+        this.setState({quotes: _.without(this.state.quotes, quote)});
     }
     render(){
-            var self = this;
+        const transitionOptions = {
+            transitionName: "fade",
+            transitionEnterTimeout: 500,
+            transitionLeaveTimeout: 500
+        }
         return (
             <div>
-            <br />
-            <div className="btn-group pull-right" role="group">
-                <button className="btn btn-default btn-warning" onClick={this.onAddList.bind(this)}>Add List</button>
-                <button className="btn btn-default btn-warning">Index</button>
-            </div>
-            <br /><br />
+            <button className="btn btn-default btn-warning" onClick={this.onAddList.bind(this)}>Add List</button>
             <ul className="list-group">
+            <ReactCSSTrasitionGroup {...transitionOptions}>
             {this.renderList()}
+            </ReactCSSTrasitionGroup>
             </ul>
             </div>
         );
