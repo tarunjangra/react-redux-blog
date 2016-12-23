cdba93d288ee2c0ced5c6f0db11119e3ff62eced
Tarun Jangra <tarun@izap.in>
2016-12-23 12:57:03 +0530

    handle react with browserHistory

--- server.js
+++ server.js
@@ -5,5 +5,13 @@ const app = express();
 
 app.use(express.static(__dirname));
 
+app.get('/api/posts' ,(req, res) =>{
+    res.send([{id:1}]);
+});
+
+app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'index.html'));
+});
+
 app.listen(port);
 console.log('Server started');
